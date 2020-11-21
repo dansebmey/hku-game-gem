a683df1afe54e7cb214fd6327bbaeb80619c5f7d
Daniel Meyer <dansebmey@gmail.com>
2020-11-21 11:43:11 +0100

    Organize code structure

--- Bed Time Story/Assets/Scripts/Entities/Props/Cube.cs
+++ Bed Time Story/Assets/Scripts/Entities/Props/Cube.cs
@@ -4,6 +4,8 @@ public class Cube : Prop
 {
     public override void OnInteract(Character actor)
     {
+        base.OnInteract(actor);
+        
         transform.position = new Vector3(transform.position.x + (actor.transform.localScale.x), transform.position.y);
     }
 }
---
+++ Bed Time Story/Assets/Scripts/Entities/Props/Prop.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Prop : MonoBehaviour
+{
+    private Vector3 targetPos;
+    private SpriteRenderer _ixSpriteRenderer;
+
+    private void Awake()
+    {
+        _ixSpriteRenderer = GetComponentsInChildren<SpriteRenderer>(true)[1];
+    }
+
+    public virtual void OnInteract(Character actor)
+    {
+        Vector3 pos = transform.position;
+        
+        targetPos = new Vector3(pos.x, pos.y * -1);
+        StartCoroutine(MoveToTargetPos());
+    }
+
+    private IEnumerator MoveToTargetPos()
+    {
+        while (targetPos != transform.position)
+        {
+            yield return new WaitForSeconds(1.0f/60);
+        }
+    }
+
+    public void OnEnterScope()
+    {
+        _ixSpriteRenderer.enabled = true;
+    }
+    
+    public void OnLeaveScope()
+    {
+        _ixSpriteRenderer.enabled = false;
+    }
+}
