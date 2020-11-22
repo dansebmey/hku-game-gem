db87c15b74fe8e07264456318de8ea0aa388926c
Daniel Meyer <dansebmey@gmail.com>
2020-11-22 19:17:20 +0100

    Start working on Audio manager

---
+++ Bed Time Story/Assets/Scripts/Meta/AudioManager/AudioManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    static AudioManager instance;
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.loop = s.loop;
+        s.source.Play();
+    }
+
+    public void Play(string name, float pitchVar)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.pitch = s.pitch + UnityEngine.Random.Range(-pitchVar, pitchVar);
+        s.source.loop = s.loop;
+        s.source.Play();
+    }
+
+    public void SetVolume(string name, float value)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        StartCoroutine(AdjustVolume(s, value, 1.5f));
+    }
+
+    private IEnumerator AdjustVolume(Sound s, float targetVolume, float fadeDuration)
+    {
+        float startVolume = s.source.volume;
+        float delta = targetVolume - startVolume;
+
+        while(Mathf.Abs(s.source.volume - targetVolume) > 0.02f)
+        {
+            s.source.volume += (0.1f / fadeDuration) * delta;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Stop();
+    }
+
+    public void PlayPitched(string name, float pitch)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.pitch = pitch;
+        s.source.loop = s.loop;
+        s.source.Play();
+    }
+
+    public void PlayPitched(string name, float pitch, float pitchVar)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.pitch = pitch + UnityEngine.Random.Range(-pitchVar, pitchVar);
+        s.source.loop = s.loop;
+        s.source.Play();
+    }
+
+    internal static AudioManager GetInstance()
+    {
+        return instance;
+    }
+}
---
+++ Bed Time Story/Assets/Scripts/Meta/AudioManager/Sound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)] public float volume = 0.75f;
+    [Range(0.5f, 1.5f)] public float pitch = 1f;
+
+    public bool loop = false;
+
+    [HideInInspector]
+    public AudioSource source;
+}
